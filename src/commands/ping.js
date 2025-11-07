829ab00c184b17f0b2ebd839ea7da2e256385d1c
Aizawa270 <aizawa260@gmail.com>
2025-11-07 14:50:56 +0600

    Delete src directory

--- src/commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Ping command',
-  async execute({ message }) {
-    const sent = await message.reply('Pinging...');
-    const diff = sent.createdTimestamp - message.createdTimestamp;
-    sent.edit(`Pong! ${diff}ms`);
-  }
-};
