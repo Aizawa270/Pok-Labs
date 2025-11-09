65214d10fcbb88dd7e52521dc9f801d9e41c5ce0
Aizawa270 <aizawa260@gmail.com>
2025-11-09 04:40:51 +0000

    Force removed corrupted catch.js file

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Ping the bot to check latency: %ping',
-  async execute({ client, message }) {
-    const sent = await message.reply('Pinging...');
-    sent.edit(`Pong! Latency: ${sent.createdTimestamp - message.createdTimestamp}ms`);
-  },
-};
